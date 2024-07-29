3eebf38f43b4a648219f73c089aa11fedb5078be
Pratik Gagare <pratik.gagare@nityaobject.com>
2024-07-29 18:34:14 +0530

    removed logger

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"github.com/pratikgagare03/feedback/handlers"
-	"github.com/pratikgagare03/feedback/logger"
 )
 
 func setupRoutes(router *gin.Engine) {
@@ -29,14 +28,11 @@ func setupRoutes(router *gin.Engine) {
 
 }
 func main() {
-	logger.Logs.Info().Msg("Started Main")
 	err := godotenv.Load(".env")
 	if err != nil {
-		logger.Logs.Error().Err(err)
+		panic(".env not found")
 	}
-	defer logger.File.Close()
 	router := gin.Default()
 	setupRoutes(router)
 	router.Run(os.Getenv("APP_PORT"))
-	logger.Logs.Info().Msg("Main Function over")
 }
